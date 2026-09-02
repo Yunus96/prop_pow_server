b2d382f27a10abe3efe6c4aa4ea4d3605823cd15
Yunus96 <mohammadyunus102@gmail.com>
2026-09-02 14:23:19 +0530

    Initial Express backend

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import cors from 'cors'
+const app = express()
+const port = 3001
+import propelAuth from '@propelauth/express'
+
+const { requireUser } = propelAuth.initAuth({
+  authUrl: process.env.PROPEL_AUTH_URL!,
+  apiKey: process.env.PROPEL_API_KEY!,
+})
+
+
+app.use(cors())
+
+app.get('/api/whoami', requireUser, (req, res) => {
+    res.send({ user: req.user?.userId })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
